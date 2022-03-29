278981268f005312623c106df005368f0ec3e670
jun-hY <57747650+jun-hY@users.noreply.github.com>
2022-03-29 11:36:43 +0900

    add chkErr, chkStatusCode

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	fmt.Println("hello, world!")
+	pages := getPages()
+}
+
+func getPages() int {
+	resp, err := http.Get(baseURL)
+	chkErr(err)
+
+	return 0
+}
+
+func chkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func chkCode(resp http.Response) {
+	if resp.StatusCode != 200 {
+		log.Fatalln("Request failed with Status : ", resp.StatusCode)
+	}
 }
