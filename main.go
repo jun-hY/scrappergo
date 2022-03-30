15157eff08c1d875c4031378967ef050802a11de
jun-hY <57747650+jun-hY@users.noreply.github.com>
2022-03-30 09:16:44 +0900

    getPages func test

--- main.go
+++ main.go
@@ -1,30 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
 	pages := getPages()
+	fmt.Println(pages)
 }
 
+// getPages is check the pages can response
 func getPages() int {
 	resp, err := http.Get(baseURL)
 	chkErr(err)
+	chkCode(resp)
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	chkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Html())
+	})
 
 	return 0
 }
 
+// chkErr is check the Error
 func chkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func chkCode(resp http.Response) {
+// chkCode is check the http status Code
+func chkCode(resp *http.Response) {
 	if resp.StatusCode != 200 {
 		log.Fatalln("Request failed with Status : ", resp.StatusCode)
 	}
