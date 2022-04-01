aaeb3e10c5304ff843d7b7ace25bc0127a662915
jun-hY <57747650+jun-hY@users.noreply.github.com>
2022-04-01 09:22:51 +0900

    getPage func is on typing
    already done!

--- main.go
+++ main.go
@@ -4,15 +4,55 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summery  string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
+	//var c chan string
 	pages := getPages()
 	fmt.Println(pages)
+	for i := 0; i < pages; i++ {
+		getPage(i)
+	}
+}
+
+// getPage get elements in page
+func getPage(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting this url :", pageURL)
+	resp, err := http.Get(pageURL)
+	chkErr(err)
+	chkCode(resp)
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	chkErr(err)
+
+	doc.Find(".result").Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		title := card.Find(".jobTitle").Text()
+		location := card.Find(".companyLocation").Text()
+		salary, _ := card.Find(".salary-snippet").Attr("aria-label")
+		if salary != "" {
+			fmt.Println(id)
+			fmt.Println(title)
+			fmt.Println(location)
+			fmt.Println(salary)
+		}
+	})
 }
 
 // getPages is check the pages can response
